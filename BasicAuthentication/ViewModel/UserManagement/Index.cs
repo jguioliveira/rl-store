f57a6504059b394594afd81b1f5885580bdd3358
Jose Guilherme de Oliveira <jgoliveira@brq.com>
2020-05-14 18:34:27 -0300

    ViewModels usadas na tela de Gerenciamento de Usuarios.

---
+++ BasicAuthentication/ViewModel/UserManagement/Index.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace BasicAuthentication.ViewModel.UserManagement
+{
+    public class Index
+    {
+        public IEnumerable<UserData> Users { get; set; }
+    }
+}
---
+++ BasicAuthentication/ViewModel/UserManagement/UserData.cs
@@ -0,0 +1,15 @@
+﻿namespace BasicAuthentication.ViewModel.UserManagement
+{
+    public class UserData
+    {
+        public string Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
