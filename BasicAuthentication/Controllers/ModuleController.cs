f5e9f165b07fe823411509e5ec3029473033532d
Jose Guilherme de Oliveira <jgoliveira@brq.com>
2020-05-14 19:26:33 -0300

    Consulta dos modulos.

--- BasicAuthentication/Controllers/ModuleController.cs
+++ BasicAuthentication/Controllers/ModuleController.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BasicAuthentication.Domain.Repositories;
+using BasicAuthentication.ViewModel.ModuleManagement;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace BasicAuthentication.Controllers
 {
     public class ModuleController : Controller
     {
-        public IActionResult Index()
+        private readonly IModuleRepository _moduleRepository;
+
+        public ModuleController(IModuleRepository moduleRepository)
         {
-            return View();
+            _moduleRepository = moduleRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var modules = await _moduleRepository.GetAsync();
+
+            Index indexModel = new Index
+            {
+                Modules = modules.Select(m => new ModuleData
+                {
+                    Id = m.Id.ToString(),
+                    Name = m.Name,
+                    Active = m.Active
+                })
+            };
+
+            return View(indexModel);
         }
 
         public IActionResult Edit()
