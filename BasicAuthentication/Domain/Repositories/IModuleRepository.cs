2fb99e9535bea0d925abf51f1854ba1d47b3b5e8
Jose Guilherme de Oliveira <jgoliveira@brq.com>
2020-05-14 19:25:47 -0300

    Repositorio de Modulos.

---
+++ BasicAuthentication/Domain/Repositories/IModuleRepository.cs
@@ -0,0 +1,19 @@
+﻿using BasicAuthentication.Domain.Entities;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BasicAuthentication.Domain.Repositories
+{
+    public interface IModuleRepository
+    {
+
+        Task<Module> GetAsync(string id);
+
+        Task<IEnumerable<Module>> GetAsync();
+
+        Task CreateAsync(Module module);
+
+        Task UpdateAsync(Module module);
+    }
+}
---
+++ BasicAuthentication/Infrastructure/Repositories/ModuleRepository.cs
@@ -0,0 +1,47 @@
+﻿using BasicAuthentication.Domain.Entities;
+using BasicAuthentication.Domain.Repositories;
+using BasicAuthentication.Infrastructure.Context;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicAuthentication.Infrastructure.Repositories
+{
+    public class ModuleRepository : IModuleRepository
+    {
+        private readonly IUserDataContext _userDataContext;
+
+        public ModuleRepository(IUserDataContext userDataContext)
+        {
+            _userDataContext = userDataContext;
+        }
+
+        public async Task CreateAsync(Module module)
+        {
+            await _userDataContext.Modules.InsertOneAsync(module);
+        }
+
+        public async Task<Module> GetAsync(string id)
+        {
+            var filter = Builders<Module>.Filter.Eq("id", id);
+            return await _userDataContext.Modules.Find(filter).SingleOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Module>> GetAsync()
+        {
+            return await _userDataContext.Modules.AsQueryable().ToListAsync();
+        }
+
+        public async Task UpdateAsync(Module module)
+        {
+            var filter = Builders<Module>.Filter.Eq("id", module.Id);
+
+            var updateDefinition = Builders<Module>.Update
+                .Set("name", module.Name)
+                .Set("active", module.Active);
+
+            await _userDataContext.Modules.UpdateOneAsync(filter, updateDefinition);
+        }
+    }
+}
