379968c9b2bd0943d16dadf1fd5ec252601cba4e
Jose Guilherme de Oliveira <jgoliveira@brq.com>
2020-05-14 18:51:54 -0300

    View para Edicao do Modulo.

--- BasicAuthentication/Controllers/ModuleController.cs
+++ BasicAuthentication/Controllers/ModuleController.cs
@@ -1,14 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace BasicAuthentication.Controllers
 {
     public class ModuleController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
         {
             return View();
         }
