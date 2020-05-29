d219d902117e13fd7a01d241faf5d312c6ce8c11
Jose Guilherme de Oliveira <jgoliveira@brq.com>
2020-05-28 23:10:12 -0300

    Configurando chaves usadas do OAuth.

---
+++ OAuthUserManagement/Configuration/Settings.cs
@@ -0,0 +1,14 @@
+﻿namespace UserManagement.OAuth.Configuration
+{
+    public class Settings
+    {
+        public OAuthSettings OAuthSettings { get; set; }
+    }
+
+    public class OAuthSettings
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string Secret { get; set; }
+    }
+}
