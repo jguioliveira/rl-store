2c94fec50a1108afec51c9c56948e5099ffed918
Jose Guilherme de Oliveira <jgoliveira@brq.com>
2020-05-30 17:51:30 -0300

    Entidade e Repositorio para controle do token de acesso.

---
+++ UserManagement.Domain/Entities/UserToken.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserManagement.Domain.Entities
+{
+    public  class UserToken
+    {
+        public UserToken(string email, string authorizationCode, string redirectUri, string authorizationCodeExpiration)
+        {
+            Email = email;
+            AuthorizationCode = authorizationCode;
+            RedirectUri = redirectUri;
+            AuthorizationCodeExpiration = authorizationCodeExpiration;
+            IsCanceled = false;
+        }
+
+        public string Id { get; private set; }
+        public string Email { get; private set; }
+        public string AuthorizationCode { get; private set; }
+        public string RedirectUri { get; private set; }
+        public string AuthorizationCodeExpiration { get; private set; }
+        public string Token { get; private set; }
+        public string TokenExpiration { get; private set; }
+        public string RefreshToken { get; private set; }
+        public bool IsCanceled { get; private set; }
+    }
+}
---
+++ UserManagement.Domain/Repositories/IUserTokenRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using UserManagement.Domain.Entities;
+
+namespace UserManagement.Domain.Repositories
+{
+    public interface IUserTokenRepository
+    {
+        Task CreateAsync(UserToken userToken);
+        Task<UserToken> GetAsync(string authorizationCode);
+        Task UpdateAsync(string id, UserToken userToken);
+    }
+}
