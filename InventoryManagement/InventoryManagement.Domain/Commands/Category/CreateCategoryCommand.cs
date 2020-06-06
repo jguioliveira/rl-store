0196d8810bc82826b4ba9d5312adead33060e563
Jose Guilherme de Oliveira <jgoliveira@brq.com>
2020-06-06 20:05:46 -0300

    Commands da entidade de Categoria.

---
+++ InventoryManagement/InventoryManagement.Domain/Commands/Category/CreateCategoryCommand.cs
@@ -0,0 +1,44 @@
+﻿using InventoryManagement.Domain.Entities;
+using InventoryManagement.Domain.Validation;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InventoryManagement.Domain.Commands
+{
+    public class CreateCategoryCommand : Validate, ICommand
+    {
+        public int MAX_SUBCATEGORIES { get => Category.MAX_SUBCATEGORIES; }
+
+        public class CategoryData
+        {
+            public string Name { get; set; }
+
+            public IEnumerable<CategoryData> Children { get; set; }
+        }
+
+        public string Name { get; set; }
+
+        public IEnumerable<CategoryData> Children { get; set; }
+
+        public void Validate()
+        {
+            if (string.IsNullOrEmpty(Name))
+                AddError("Invalid name.");
+
+            static bool childrenNameEmpty(IEnumerable<CategoryData> categories)
+            {
+                bool result = categories.Any(c => string.IsNullOrEmpty(c.Name));
+
+                if (!result)
+                {
+                    result = childrenNameEmpty(categories.SelectMany(c => c.Children));
+                }
+
+                return result;
+            }
+
+            if (!(Children is null) && childrenNameEmpty(Children))
+                AddError("Invalid children name.");
+        }
+    }
+}
---
+++ InventoryManagement/InventoryManagement.Domain/Commands/Category/UpdateCategoryCommand.cs
@@ -0,0 +1,20 @@
+﻿using InventoryManagement.Domain.Validation;
+
+namespace InventoryManagement.Domain.Commands
+{
+    public class UpdateCategoryCommand : Validate, ICommand
+    {
+        public string Id { get; set; }
+        public string FatherCategoryId { get; set; }
+        public string Name { get; set; }
+
+        public void Validate()
+        {
+            if (string.IsNullOrEmpty(Name))
+                AddError("Invalid name.");
+
+            if (string.IsNullOrEmpty(Id))
+                AddError("Invalid id.");
+        }
+    }
+}
