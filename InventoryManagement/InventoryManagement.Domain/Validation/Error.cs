b4d914101c09cb50ff38d93b4e320e87dac0e36e
Jose Guilherme de Oliveira <jgoliveira@brq.com>
2020-06-06 20:04:37 -0300

    Classes para auxiliar nas validações.

---
+++ InventoryManagement/InventoryManagement.Domain/Validation/Error.cs
@@ -0,0 +1,12 @@
+﻿namespace InventoryManagement.Domain.Validation
+{
+    public sealed class Error
+    {
+        public Error(string message)
+        {
+            Message = message;
+        }
+
+        public string Message { get; private set; }
+    }
+}
---
+++ InventoryManagement/InventoryManagement.Domain/Validation/Validate.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace InventoryManagement.Domain.Validation
+{
+    public abstract class Validate
+    {
+        private readonly List<Error> _errors;
+
+        protected Validate()
+        {
+            _errors = new List<Error>();
+        }
+
+        public IReadOnlyCollection<Error> Errors => _errors;
+
+        public void AddError(string message)
+        {
+            _errors.Add(new Error(message));
+        }
+
+        public void AddError(Error error)
+        {
+            _errors.Add(error);
+        }
+
+        public void AddError(IEnumerable<Error> errors)
+        {
+            _errors.AddRange(errors);
+        }
+
+        public void AddError(Validate item)
+        {
+            AddError(item.Errors);
+        }
+
+        public void AddError(params Validate[] items)
+        {
+            foreach (var item in items)
+                AddError(item);
+        }
+
+        public void Clear()
+        {
+            _errors.Clear();
+        }
+
+        public bool IsValid => !_errors.Any();
+    }
+}
