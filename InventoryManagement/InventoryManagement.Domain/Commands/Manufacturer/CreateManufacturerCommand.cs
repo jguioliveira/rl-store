e926a4dc887e1033a641c7d67cdd79a00e39cf5d
Jose Guilherme de Oliveira <jgoliveira@brq.com>
2020-06-06 20:05:56 -0300

    Commands da entidade de Fabricante.

---
+++ InventoryManagement/InventoryManagement.Domain/Commands/Manufacturer/CreateManufacturerCommand.cs
@@ -0,0 +1,17 @@
+﻿using InventoryManagement.Domain.Validation;
+
+namespace InventoryManagement.Domain.Commands
+{
+    public class CreateManufacturerCommand : Validate, ICommand
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        public void Validate()
+        {
+            if(string.IsNullOrEmpty(Name))
+                AddError("Invalid Name.");
+        }
+    }
+}
---
+++ InventoryManagement/InventoryManagement.Domain/Commands/Manufacturer/UpdateManufacturerCommand.cs
@@ -0,0 +1,22 @@
+﻿using InventoryManagement.Domain.Validation;
+using System;
+
+namespace InventoryManagement.Domain.Commands
+{
+    public class UpdateManufacturerCommand : Validate, ICommand
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        public void Validate()
+        {
+            if (string.IsNullOrEmpty(Id))
+                AddError("Invalid Id.");
+
+            if (string.IsNullOrEmpty(Name))
+                AddError("Invalid Name.");
+        }
+    }
+}
