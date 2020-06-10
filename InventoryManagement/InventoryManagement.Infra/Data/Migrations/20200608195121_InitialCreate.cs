f5ae8e94fffe5ccf360f02c34f2931a9fab48e04
Jose Guilherme de Oliveira <jgoliveira@brq.com>
2020-06-10 15:13:55 -0300

    EF Migration Initial Create.

---
+++ InventoryManagement/InventoryManagement.Infra/Data/Migrations/20200608195121_InitialCreate.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InventoryManagement.Infrastructure.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<string>(maxLength: 36, nullable: false),
+                    FatherCategoryId = table.Column<string>(unicode: false, nullable: true),
+                    Name = table.Column<string>(unicode: false, maxLength: 40, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Manufacturers",
+                columns: table => new
+                {
+                    Id = table.Column<string>(maxLength: 36, nullable: false),
+                    Name = table.Column<string>(unicode: false, maxLength: 50, nullable: false),
+                    Email = table.Column<string>(unicode: false, maxLength: 50, nullable: true),
+                    Phone = table.Column<string>(unicode: false, maxLength: 20, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Manufacturers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<string>(maxLength: 36, nullable: false),
+                    Code = table.Column<string>(unicode: false, maxLength: 20, nullable: false),
+                    Name = table.Column<string>(unicode: false, maxLength: 50, nullable: false),
+                    CategoryId = table.Column<string>(nullable: true),
+                    ManufacturerId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Product_Category",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Product_Manufacturer",
+                        column: x => x.ManufacturerId,
+                        principalTable: "Manufacturers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Inventories",
+                columns: table => new
+                {
+                    ProductId = table.Column<string>(maxLength: 36, nullable: false),
+                    Count = table.Column<short>(nullable: false),
+                    MinCount = table.Column<byte>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime", nullable: false),
+                    LastChange = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inventories", x => x.ProductId);
+                    table.ForeignKey(
+                        name: "FK_Inventories_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductBookMarks",
+                columns: table => new
+                {
+                    ProductId = table.Column<string>(maxLength: 36, nullable: false),
+                    Code = table.Column<string>(unicode: false, maxLength: 20, nullable: false),
+                    Name = table.Column<string>(unicode: false, maxLength: 50, nullable: false),
+                    Value = table.Column<string>(unicode: false, maxLength: 30, nullable: false),
+                    Count = table.Column<short>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductBookMarks", x => new { x.ProductId, x.Code });
+                    table.ForeignKey(
+                        name: "FK_ProductBookMarks_Product",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ManufacturerId",
+                table: "Products",
+                column: "ManufacturerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inventories");
+
+            migrationBuilder.DropTable(
+                name: "ProductBookMarks");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Manufacturers");
+        }
+    }
+}
