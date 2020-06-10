65e9e0f9edf0f54d95de50f1434c09dae61d63c0
Jose Guilherme de Oliveira <jgoliveira@brq.com>
2020-06-10 15:29:29 -0300

    Contoller de Categoria com implementação d Métodos post e put.

---
+++ InventoryManagement/InventoryManagement.Api/Controllers/v1/CategoryController.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using InventoryManagement.Domain.Commands;
+using InventoryManagement.Domain.Handlers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagement.Api.Controllers.v1
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+
+    public class CategoryController : ControllerBase
+    {
+        private readonly IHandler<CreateCategoryCommand> _createHandler;
+        private readonly IHandler<UpdateCategoryCommand> _updateHandler;
+
+        public CategoryController(
+            IHandler<CreateCategoryCommand> createHandler,
+            IHandler<UpdateCategoryCommand> updateHandler)
+        {
+            _createHandler = createHandler;
+            _updateHandler = updateHandler;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(CreateCategoryCommand createCommand)
+        {
+            var result = await _createHandler.HandleAsync(createCommand);
+
+            if (!result.Success)
+            {
+                return BadRequest(result.Message);
+            }
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync(UpdateCategoryCommand updateCommand)
+        {
+            var result = await _updateHandler.HandleAsync(updateCommand);
+
+            if (!result.Success)
+            {
+                return BadRequest(result.Message);
+            }
+
+            return Ok();
+        }
+    }
+}
