0d523a595274d76039b5a8370d865e19edce62cc
Jose Guilherme de Oliveira <jgoliveira@brq.com>
2020-06-10 15:29:39 -0300

    Contoller de Fabricante com implementação d Métodos post e put.

---
+++ InventoryManagement/InventoryManagement.Api/Controllers/v1/ManufacturerController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using InventoryManagement.Domain.Commands;
+using InventoryManagement.Domain.Handlers;
+
+namespace InventoryManagement.Api.Controllers.v1
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class ManufacturerController : ControllerBase
+    {
+        private readonly IHandler<CreateManufacturerCommand> _createHandler;
+        private readonly IHandler<UpdateManufacturerCommand> _updateHandler;
+
+        public ManufacturerController(
+            IHandler<CreateManufacturerCommand> createHandler,
+            IHandler<UpdateManufacturerCommand> updateHandler)
+        {
+            _createHandler = createHandler;
+            _updateHandler = updateHandler;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(CreateManufacturerCommand createCommand)
+        {
+            var result = await _createHandler.HandleAsync(createCommand);
+
+            if (!result.Success)
+            {
+                return BadRequest(result.Message);
+            }
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync(UpdateManufacturerCommand updateCommand)
+        {
+            var result = await _updateHandler.HandleAsync(updateCommand);
+
+            if (!result.Success)
+            {
+                return BadRequest(result.Message);
+            }
+
+            return Ok();
+        }
+    }
+}
