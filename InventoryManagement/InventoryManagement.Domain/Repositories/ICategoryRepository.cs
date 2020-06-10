adccddd6511cb4cf075bf04fb4c3ed741121a41b
Jose Guilherme de Oliveira <jgoliveira@brq.com>
2020-06-10 15:15:35 -0300

    Removendo parametro id do método update.

--- InventoryManagement/InventoryManagement.Domain/Repositories/ICategoryRepository.cs
+++ InventoryManagement/InventoryManagement.Domain/Repositories/ICategoryRepository.cs
@@ -7,7 +7,7 @@ namespace InventoryManagement.Domain.Repositories
     public interface ICategoryRepository
     {
         Task CreateAsync(IEnumerable<Category> categories);
-        Task UpdateAsync(string id, Category category);
+        Task UpdateAsync(Category category);
         Task<bool> ContainsAsync(string name);
         Task<bool> ContainsAtAsync(string fatherCategoryId, string name);
         Task<bool> ExistsAsync(string id);
