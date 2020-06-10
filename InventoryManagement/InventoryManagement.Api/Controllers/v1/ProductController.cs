8478899096a87a617c0d19a3c7b89734daee4c18
Jose Guilherme de Oliveira <jgoliveira@brq.com>
2020-06-10 15:29:55 -0300

    Contoller de Produto com implementação dos Métodos post e put.

---
+++ InventoryManagement/InventoryManagement.Api/Controllers/v1/ProductController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InventoryManagement.Domain.Commands;
+using InventoryManagement.Domain.Handlers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagement.Api.Controllers.v1
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IHandler<CreateProductCommand> _createHandler;
+        private readonly IHandler<UpdateProductCommand> _updateHandler;
+        private readonly IHandler<UpdateInventoryCommand> _updateInventoryHandler;
+        private readonly IHandler<UpdateBookMarksCommand> _updateBookMarksHandler;
+
+        public ProductController(
+            IHandler<CreateProductCommand> createHandler,
+            IHandler<UpdateProductCommand> updateHandler,
+            IHandler<UpdateInventoryCommand> updateInventoryHandler,
+            IHandler<UpdateBookMarksCommand> updateBookMarksHandler)
+        {
+            _createHandler = createHandler;
+            _updateHandler = updateHandler;
+            _updateInventoryHandler = updateInventoryHandler;
+            _updateBookMarksHandler = updateBookMarksHandler;
+        }
+
+        [HttpGet]        
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(CreateProductCommand createCommand)
+        {
+            var result = await _createHandler.HandleAsync(createCommand);
+
+            if (!result.Success)
+            {
+                return BadRequest(result.Message);
+            }
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync(UpdateProductCommand updateCommand)
+        {
+            var result = await _updateHandler.HandleAsync(updateCommand);
+
+            if (!result.Success)
+            {
+                return BadRequest(result.Message);
+            }
+
+            return Ok();
+        }
+
+        [HttpPut("{id}/inventory")]
+        public async Task<IActionResult> UpdateInventoryAsync(string id, UpdateInventoryCommand updateCommand)
+        {
+            if (string.IsNullOrEmpty(updateCommand.ProductId))
+                updateCommand.ProductId = id;
+
+            var result = await _updateInventoryHandler.HandleAsync(updateCommand);
+
+            if (!result.Success)
+            {
+                return BadRequest(result.Message);
+            }
+
+            return Ok();
+        }
+
+        [HttpPut("{id}/productbookmark")]
+        public async Task<IActionResult> UpdateProductBookMarkAsync(string id, UpdateBookMarksCommand updateCommand)
+        {
+            if (string.IsNullOrEmpty(updateCommand.ProductId))
+                updateCommand.ProductId = id;
+
+            var result = await _updateBookMarksHandler.HandleAsync(updateCommand);
+
+            if (!result.Success)
+            {
+                return BadRequest(result.Message);
+            }
+
+            return Ok();
+        }
+    }
+}
