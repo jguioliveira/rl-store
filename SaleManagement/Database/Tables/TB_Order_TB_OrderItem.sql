cf8d0f1ceb49b477bba3a3d4402bbb6eefb25316
Diego Sumiya <diego.sumiya@gmail.com>
2020-08-08 22:25:23 -0300

    Criacao da database RLSalesDB e criacao das tabelas TB_Order e TB_OrderItem

---
+++ SaleManagement/Database/Tables/TB_Order_TB_OrderItem.sql
@@ -0,0 +1,21 @@
+create database RLSalesDB;
+create table TB_Order(
+	Id varchar(32)not null,
+    Custmomerid varchar(32),
+    Status tinyint,
+    Total decimal(14,2)not null,
+    Created date not null,
+    Updated date not null,
+    PaymentForm tinyint,
+    primary key(Id)
+);
+
+create table TB_OrderItem(
+	OrderId varchar(32),
+    ProductId varchar(32),
+    Count integer,
+    UnitValue decimal(14,2)not null,
+    Total decimal(14,2)not null,
+    ProductName varchar(100),
+    primary key(OrderId, ProductId)
+);
