ae45e9b8b4dbf91d0765f1ad7346b46f15b651bc
Diego Sumiya <diego.sumiya@gmail.com>
2020-08-16 23:19:43 -0300

    Criacao da pasta Testes, teste realizado com todas as procedures

---
+++ SaleManagement/Database/Testes/Teste_PR_TB_OrderItem_Insert.sql
@@ -0,0 +1,6 @@
+call PR_TB_OrderItem_Insert(1, 1, 2, 10.00, 20.00, 'lapis');
+call PR_TB_OrderItem_Insert(2, 2, 1, 12.00, 12.00, 'Caderno');
+call PR_TB_OrderItem_Insert(3, 3, 3, 5.00, 15.00, 'Borracha');
+call PR_TB_OrderItem_Insert(4, 4, 1, 50.00, 50.00, 'Mochila');
+call PR_TB_OrderItem_Insert(5, 5, 1, 15.00, 15.00, 'Estojo');
+
